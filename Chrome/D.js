835404b988596902852bfed176847d535e6d45a1
XhstormR <674080894@qq.com>
2016-12-29 23:48:43 +0800

    Signed-off-by: XhstormR <X>

--- Chrome/D.js
+++ Chrome/D.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function(event) {
     window.setTimeout(function() {
         document.getElementById('x-video-button').dispatchEvent(new UIEvent('touchend',{bubbles: true}));
-    }, 300);
+    }, 400);
 });
