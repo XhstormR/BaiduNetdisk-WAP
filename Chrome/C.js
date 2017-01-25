203f41d80bc3ca04fbc48d37afd93a896c7d1a35
XhstormR <674080894@qq.com>
2017-01-25 09:56:55 +0800

    Signed-off-by: XhstormR <X>

--- Chrome/C.js
+++ Chrome/C.js
@@ -11,3 +11,21 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 }, {
     urls: ['*://*.v2ex.com/*', '*://*.zhihu.com/*', '*://*.solidot.org/*', '*://*.vmovier.com/*', '*://v.youku.com/*'] //    urls: ['<all_urls>']
 }, ['blocking', 'requestHeaders']);
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    if (!confirm('是否要关闭重复标签页？'))
+        return;
+    var urls = [], tabsToClose = [];
+    chrome.tabs.query({
+        currentWindow: true
+    }, function(tabs) {
+        tabs.reverse().forEach(function(tab) {
+            if (~urls.indexOf(tab.url)) {
+                tabsToClose.push(tab.id);
+            } else {
+                urls.push(tab.url);
+            }
+        });
+        chrome.tabs.remove(tabsToClose);
+    });
+});
