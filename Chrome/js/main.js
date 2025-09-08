2737834ce096998e1eceb9cbfb5c8e42914cf890
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-09-08 06:53:39 +0300

    Update

--- Chrome/js/main.js
+++ Chrome/js/main.js
@@ -23,25 +23,31 @@ function translate(canRead) {
         return;
     }
     if (canRead) {
-        let utterance = new SpeechSynthesisUtterance(text);
-        utterance.lang = 'en-US';
-        utterance.rate = 0.7;
-        speechSynthesis.speak(utterance);
+        read(text);
     }
     chrome.runtime.sendMessage({text: text, canRead: canRead}, log);
 }
 
 function log(json) {
-    window.console.group('%c%s', CSS, json[5][0][0]);
+    console.group('%c%s', CSS, Date.now());
     if (json.translationResponse !== undefined) {
-        window.console.log('%c%s', CSS, json.translationResponse);
+        console.log('%c%s', CSS, json.translationResponse);
     } else {
         if (json[1]) {
-            json[1].forEach(value => window.console.log('%c%s', CSS, resolve(value[0]) + reduce(value[1])));
+            json[1].forEach(value => console.log('%c%s', CSS, resolve(value[0]) + reduce(value[1])));
         }
-        json[0].forEach(value => window.console.log('%c%s', CSS, value[0] !== null ? value[0] : ''));
+        json[0]
+            .filter(value => value[0] != null)
+            .forEach(value => console.log('%c%s', CSS, value[1] + '\n' + value[0]));
     }
-    window.console.groupEnd();
+    console.groupEnd();
+}
+
+function read(text) {
+    let utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'en-GB';
+    utterance.rate = 0.7;
+    speechSynthesis.speak(utterance);
 }
 
 function resolve(str) {
